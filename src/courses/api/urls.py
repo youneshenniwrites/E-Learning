d2cd8626c51c4308eea8a546df98e4f3027259bb
Younes Henni <henni1989younes@gmail.com>
2017-10-16 11:46:01 +0100

    Adding courses api views and viewsets with dynamic url routers

--- src/courses/api/urls.py
+++ src/courses/api/urls.py
@@ -1,12 +1,22 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from rest_framework import routers
 
 from . import views
 
 
+# automatic generation of URLs for the viewset 
+router = routers.DefaultRouter()
+router.register('courses', views.CourseViewSet)
+
 urlpatterns = [url(r'^subjects/$',
                     views.SubjectListView.as_view(),
                     name='subject-list'),
-               url( r'^subjects/(?P<pk>\d+)/$',
+                url(r'^subjects/(?P<pk>\d+)/$',
                     views.SubjectDetailView.as_view(),
                     name ='subject-detail'),
+                url(r'^courses/(?P<pk>\d+)/enroll/$',
+                     views.CourseEnrollView.as_view(),
+                     name ='course-enroll'),
+                url(r'', include(router.urls))
                 ]
--- src/courses/api/views.py
+++ src/courses/api/views.py
@@ -1,7 +1,34 @@
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
 
-from ..models import Subject
-from .serializers import SubjectSerializer
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+
+from ..models import Subject, Course
+from .serializers import SubjectSerializer, CourseSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    '''
+    API viewset to both list objects
+    and retrieve a single objects
+    '''
+    
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class CourseEnrollView(APIView):
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.users.add(request.user)
+        return Response({'enrolled': True})
 
 
 class SubjectListView(generics.ListAPIView):
